3ba0005218f8c94bed70bde56798cf36217a9655
mike <kma1660@gmail.com>
2016-07-13 14:29:50 -0400

    forgot :(

--- navigator-shooter.ino
+++ navigator-shooter.ino
@@ -1,7 +1,8 @@
 #include <Servo.h>
-
+//#define USBCON
 #include <ros.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
 
 class Shooter
 {
@@ -43,25 +44,28 @@ class Shooter
 int Shooter::goal = 0;
 Servo Shooter::controller = Servo();
 
+char hello[13] = "hello world";
 class Comms
 {
   private:
     ros::NodeHandle nh;
     ros::Subscriber<std_msgs::Bool> subscriber;
-    //std_msgs::String str_msg;
-    //ros::Publisher chatter;
+    std_msgs::String str_msg;
+    ros::Publisher chatter;
     static const char ON_CHAR = 'i';
     static const char OFF_CHAR = 'o';
   public:
     Comms() : 
       nh(),
-      subscriber("shooter",&callback)
-      //str_msg(),
-      //chatter("chatter", &str_msg)
+      subscriber("shooter",&callback),
+      str_msg(),
+      chatter("chatter", &str_msg)
     {
+      nh.getHardware()->setBaud(9600);
       nh.initNode();
-      //nh.advertise(chatter);
-      nh.subscribe(subscriber);
+      //delay(3000);
+      nh.advertise(chatter);
+      //nh.subscribe(subscriber);
     }
     static void callback(const std_msgs::Bool& msg)
     {
@@ -70,6 +74,8 @@ class Comms
     }
     void run()
     {
+      str_msg.data = hello;
+      chatter.publish(&str_msg);
       nh.spinOnce();
     }
 };
@@ -77,14 +83,17 @@ class Comms
 Comms com;
 void setup()
 {
+  pinMode(13,OUTPUT);
   com = Comms();
-  Shooter::init();
+  //Shooter::init();
+  //delay(500);
 }
 
 void loop()
 {
   com.run();
-  Shooter::run();
-  delay(50);
+  //Shooter::run();
+  delay(1000);
 }
+
 
